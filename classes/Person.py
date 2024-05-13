51373dcfe698cad9f98b6762cd7dd98c251ad7c6
Lautaro Demonte <lauty.d.p@gmail.com>
2024-05-13 17:40:58 -0300

    creo la clase Person con sus atributos y metodos

---
+++ classes/Person.py
@@ -0,0 +1,22 @@
+class Person():
+    def __init__(self, _id, name, age, products):
+        self._id = _id
+        self.name = name
+        self.age = age
+        self.products = []
+
+    def __str__(self):
+        return f'My id is {self._id} and this is my cart {self.products}'
+    
+    def __len__(self):
+        return f'I have {len(self.products)}'
+
+    def add_to_cart(self, product):
+        self.products.append(product)
+
+    def remove_from_cart(self, product):
+        self.products.remove(product)
+
+    def buy(self):
+      print("Purchase successful!")
+      self.products = []
